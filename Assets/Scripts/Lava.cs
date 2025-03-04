66ea353a07b011c3ce6afe796faadec905d50d81
lucialv <lucia.alvrzt@gmail.com>
2025-03-04 14:37:02 +0100

    fixed

--- Assets/Scripts/Lava.cs
+++ Assets/Scripts/Lava.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class Lava : MonoBehaviour
 {
+    [SerializeField] 
+    private GameObject tpPoint;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             //Tp the player to the 0.0 position
-            collision.gameObject.transform.position = new Vector3(10.747f, 10.675f, 0);
+            collision.gameObject.transform.position = tpPoint.transform.position;
             //Make the player take damage
             collision.gameObject.GetComponent<PlayerManager>().TakeDamage(1);
         }
